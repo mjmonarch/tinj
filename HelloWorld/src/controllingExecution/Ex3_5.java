1ff1084e3c89a347522b3caa4c011c6d3654d343
mjmonarch <mjmonarch@gmail.com>
2015-08-18 21:34:32 +0300

    18082015-2

---
+++ HelloWorld/src/controllingExecution/Ex3_5.java
@@ -0,0 +1,22 @@
+package controllingExecution;
+
+public class Ex3_5 {
+
+	public static String printBinary (int value) {
+		String result = "";
+		int value1 = value;
+		if (value > 0) {
+			while ((long) value*value1 > 0) {
+				result += (value1%2 == 0) ? "0" : "1";
+				value1 >>= 1;
+			}
+		}
+		else if (value < 0 ) {
+			result = "-1";
+		}
+		else return "0";
+		return result;
+	}
+
+	
+}
